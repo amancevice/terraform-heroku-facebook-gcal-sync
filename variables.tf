e310f72d22479f1fe825994f6b9d762ca97884fd
Alexander Mancevice <amancevice@cargometrics.com>
2020-03-21 10:20:54 -0400

    terraform 0.12 updates

--- variables.tf
+++ variables.tf
@@ -1,60 +1,60 @@
-variable heroku_email {
-  description = "Heroku account email."
-  default     = ""
-}
-
-variable heroku_api_key {
-  description = "Heroku account API key."
-  default     = ""
+variable app_buildpacks {
+  description = "Heroku app buildpacks"
+  default     = ["heroku/python"]
+  type        = list(string)
 }
 
 variable app_name {
-  description = "Heroku app name."
+  description = "Heroku app name"
 }
 
 variable app_region {
-  description = "Heroku app region."
+  description = "Heroku app region"
   default     = "us"
 }
 
 variable app_stack {
-  description = "Heroku app stack."
+  description = "Heroku app stack"
   default     = "heroku-18"
 }
 
-variable app_buildpacks {
-  description = "Heroku app buildpacks."
-  default     = ["heroku/python"]
-  type        = "list"
+variable app_version {
+  description = "Version of module"
+  default     = "0.1.0"
 }
 
 variable facebook_page_id {
-  description = "facebook page ID."
-}
-
-variable google_calendar_id {
-  description = "Google Calendar ID."
+  description = "facebook page ID"
 }
 
 variable facebook_page_token {
-  description = "facebook page access token."
+  description = "facebook page access token"
+}
+
+variable google_calendar_id {
+  description = "Google Calendar ID"
 }
 
 variable google_credentials_file {
-  description = "Google Service account credentials file path."
+  description = "Google Service account credentials file path"
 }
 
-variable scheduler_plan {
-  description = "Heroku scheduler addon plan."
-  default     = "scheduler:standard"
+variable heroku_email {
+  description = "Heroku account email"
+  default     = null
+}
+
+variable heroku_api_key {
+  description = "Heroku account API key"
+  default     = null
 }
 
 variable papertrail_plan {
-  description = "PaperTrail add on plan."
+  description = "PaperTrail add on plan"
   default     = "papertrail:choklad"
 }
 
-variable version {
-  description = "Version of module."
-  default     = "0.1.0"
+variable scheduler_plan {
+  description = "Heroku scheduler addon plan"
+  default     = "scheduler:standard"
 }
