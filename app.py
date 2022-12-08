f36c5a49277e4ecc100799742082e713b5757f44
Alexander Mancevice <alexander.mancevice@hey.com>
2022-12-08 09:57:52 -0500

    black formatting

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ from googleapiclient import discovery
 
 import fest
 
-DRYRUN = str(os.getenv('DRYRUN')).lower() in ['true', '1']
-FACEBOOK_PAGE_ID = os.environ['FACEBOOK_PAGE_ID']
-GOOGLE_CALENDAR_ID = os.environ['GOOGLE_CALENDAR_ID']
+DRYRUN = str(os.getenv("DRYRUN")).lower() in ["true", "1"]
+FACEBOOK_PAGE_ID = os.environ["FACEBOOK_PAGE_ID"]
+GOOGLE_CALENDAR_ID = os.environ["GOOGLE_CALENDAR_ID"]
 
 # Get facebook/Google secrets
-FACEBOOK_PAGE_TOKEN = os.environ['FACEBOOK_PAGE_TOKEN']
+FACEBOOK_PAGE_TOKEN = os.environ["FACEBOOK_PAGE_TOKEN"]
 GOOGLE_SERVICE_ACCOUNT = json.loads(
-    base64.b64decode(os.environ['GOOGLE_SERVICE_ACCOUNT']).decode()
+    base64.b64decode(os.environ["GOOGLE_SERVICE_ACCOUNT"]).decode()
 )
 GOOGLE_CREDENTIALS = service_account.Credentials.from_service_account_info(
     GOOGLE_SERVICE_ACCOUNT
@@ -25,13 +25,14 @@ GOOGLE_CREDENTIALS = service_account.Credentials.from_service_account_info(
 # Get facebook/Google clients
 GRAPHAPI = facebook.GraphAPI(FACEBOOK_PAGE_TOKEN)
 CALENDARAPI = discovery.build(
-    'calendar', 'v3',
+    "calendar",
+    "v3",
     cache_discovery=False,
     credentials=GOOGLE_CREDENTIALS,
 )
 
 # Configure logging
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s")
 
 
 def main(page_id=None, cal_id=None, dryrun=False):
@@ -41,22 +42,22 @@ def main(page_id=None, cal_id=None, dryrun=False):
     # Initialize facebook page & Google Calendar
     page = fest.FacebookPage(GRAPHAPI, page_id)
     gcal = fest.GoogleCalendar(CALENDARAPI, cal_id)
-    page.logger.setLevel('INFO')
-    gcal.logger.setLevel('INFO')
+    page.logger.setLevel("INFO")
+    gcal.logger.setLevel("INFO")
 
     # Sync
-    sync = gcal.sync(page, time_filter='upcoming').execute(dryrun=dryrun)
+    sync = gcal.sync(page, time_filter="upcoming").execute(dryrun=dryrun)
 
     # Return referces to modified objects
     resp = {
         k: [
             {
-                'google_id': x.get('id'),
-                'location': x.get('location'),
-                'summary': x.get('summary'),
-                'htmlLink': x.get('htmlLink'),
-                'start': event_time(x.get('start')),
-                'end': event_time(x.get('end')),
+                "google_id": x.get("id"),
+                "location": x.get("location"),
+                "summary": x.get("summary"),
+                "htmlLink": x.get("htmlLink"),
+                "start": event_time(x.get("start")),
+                "end": event_time(x.get("end")),
             }
             for facebook_id, x in v.items()
         ]
@@ -67,10 +68,10 @@ def main(page_id=None, cal_id=None, dryrun=False):
 
 def event_time(time):
     try:
-        return time['dateTime']
+        return time["dateTime"]
     except KeyError:
-        return time['date']
+        return time["date"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(json.dumps(main(), indent=2, sort_keys=True))
